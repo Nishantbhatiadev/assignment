2a9c667e4a25b547a1e29c50cdcc5c8b7cf2905d
Nishant bhatia <developernishant0708@gmail.com>
2025-04-17 14:50:38 +0530

    create an assignment

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DynamicForm from './component/DynamicForm'
+
+const App = () => {
+  return (
+    <div>
+      <DynamicForm />
+    </div>
+  )
+}
+
+export default App
